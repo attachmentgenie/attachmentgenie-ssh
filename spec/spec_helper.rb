d688cfb7d6762f0c2c9c864a199852531f02a49f
Bram Vogelaar <attachmentgenie@inuits.eu>
2014-07-31 12:56:32 +0200

    trying to make travis happy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,25 @@
-require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
-require 'rspec-puppet'
 
 RSpec.configure do |c|
-  c.module_path = File.expand_path(File.join(__FILE__, '../../../'))
-  puts 'module_path:'+c.module_path
-  c.manifest_dir = File.expand_path(File.join(__FILE__, '..', 'fixtures/manifests'))
-  puts 'manifest_dir:'+c.manifest_dir
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+
+  c.before :each do
+    # Ensure that we don't accidentally cache facts and environment
+    # between test cases.
+    Facter::Util::Loader.any_instance.stubs(:load_all)
+    Facter.clear
+    Facter.clear_messages
+
+    # Store any environment variables away to be restored later
+    @old_env = {}
+    ENV.each_key {|k| @old_env[k] = ENV[k]}
+
+    if ENV['STRICT_VARIABLES'] == 'yes'
+      Puppet.settings[:strict_variables]=true
+    end
+  end
+end
+
+shared_examples :compile, :compile => true do
+  it { should compile.with_all_deps }
 end
