70fc2e5affd2e9d8c50080ddb8e413d41a57a97d
rexcze <rexcze@gmail.com>
2014-08-28 10:19:57 +0200

    Update client.pp
    Added support for RedHat

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,5 +1,10 @@
 class ssh::client inherits ssh::params {
-  package { 'openssh-client':
+  case $::osfamily {
+      RedHat: { $sshclientpkg = 'openssh-clients' }
+      Debian: { $sshclientpkg = 'openssh-client' }
+      default: { fail("Unsupported osfamily ${::osfamily}, currently only supports Debian and RedHat") }
+      }
+  package { $sshclientpkg:
     ensure => present,
   }
 }
