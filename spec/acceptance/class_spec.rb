f2000f4c6af60e45e4b22d070ba55cd74a1d2ed2
Bram Vogelaar <bram@attachmentgenie.com>
2021-08-26 22:22:15 +0200

    initial litmus framwork

---
+++ spec/acceptance/class_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'spec_helper_acceptance'
+
+describe 'with default parameters ', if: ['debian', 'redhat', 'ubuntu'].include?(os[:family]) do
+  pp = <<-PUPPETCODE
+  class { 'ssh::client': }
+  -> class { 'ssh::server':
+    allowed_users => ['vagrant'],
+  }
+PUPPETCODE
+
+  describe file('/home/vagrant/.ssh/authorized_keys') do
+    it { is_expected.to be_file }
+  end
+end
